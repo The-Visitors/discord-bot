7f892ec800c00ddcc0b1f21cffcb17da3d4d656a
Jacob DeHart <jacobd@jacobdehart.com>
2021-11-26 20:47:01 -0600

    set api key header

--- checkSales.ts
+++ checkSales.ts
@@ -6,12 +6,12 @@ import { ethers } from "ethers";
 const OPENSEA_SHARED_STOREFRONT_ADDRESS = '0x495f947276749Ce646f68AC8c248420045cb7b5e';
 
 const discordBot = new Discord.Client();
-const  discordSetup = async (): Promise<TextChannel> => {
+const discordSetup = async (): Promise<TextChannel> => {
   return new Promise<TextChannel>((resolve, reject) => {
     ['DISCORD_BOT_TOKEN', 'DISCORD_CHANNEL_ID'].forEach((envVar) => {
       if (!process.env[envVar]) reject(`${envVar} not set`)
     })
-  
+
     discordBot.login(process.env.DISCORD_BOT_TOKEN);
     discordBot.on('ready', async () => {
       const channel = await discordBot.channels.fetch(process.env.DISCORD_CHANNEL_ID!);
@@ -22,52 +22,55 @@ const  discordSetup = async (): Promise<TextChannel> => {
 
 const buildMessage = (sale: any) => (
   new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle(sale.asset.name + ' sold!')
-	.setURL(sale.asset.permalink)
-	.setAuthor('OpenSea Bot', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png', 'https://github.com/sbauch/opensea-discord-bot')
-	.setThumbnail(sale.asset.collection.image_url)
-	.addFields(
-		{ name: 'Name', value: sale.asset.name },
-		{ name: 'Amount', value: `${ethers.utils.formatEther(sale.total_price || '0')}${ethers.constants.EtherSymbol}`},
-		{ name: 'Buyer', value: sale?.winner_account?.address, },
-		{ name: 'Seller', value: sale?.seller?.address,  },
-	)
-  .setImage(sale.asset.image_url)
-	.setTimestamp(Date.parse(`${sale?.created_date}Z`))
-	.setFooter('Sold on OpenSea', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png')
+    .setColor('#0099ff')
+    .setTitle(sale.asset.name + ' sold!')
+    .setURL(sale.asset.permalink)
+    .setAuthor('OpenSea Bot', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png', 'https://github.com/sbauch/opensea-discord-bot')
+    .setThumbnail(sale.asset.collection.image_url)
+    .addFields(
+      { name: 'Name', value: sale.asset.name },
+      { name: 'Amount', value: `${ethers.utils.formatEther(sale.total_price || '0')}${ethers.constants.EtherSymbol}` },
+      { name: 'Buyer', value: sale?.winner_account?.address, },
+      { name: 'Seller', value: sale?.seller?.address, },
+    )
+    .setImage(sale.asset.image_url)
+    .setTimestamp(Date.parse(`${sale?.created_date}Z`))
+    .setFooter('Sold on OpenSea', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png')
 )
 
 async function main() {
   const channel = await discordSetup();
   const seconds = process.env.SECONDS ? parseInt(process.env.SECONDS) : (60 * 10);
   const hoursAgo = (Math.round(new Date().getTime() / 1000) - (seconds)); // in the last hour, run hourly?
-  
+
   const params = new URLSearchParams({
     offset: '0',
     event_type: 'successful',
     only_opensea: 'false',
-    occurred_after: hoursAgo.toString(), 
+    occurred_after: hoursAgo.toString(),
     collection_slug: process.env.COLLECTION_SLUG!,
-  })
+  });
+  const headers = {
+    'X-API-KEY': process.env.OPENSEA_API
+  };
 
   // if (process.env.CONTRACT_ADDRESS !== OPENSEA_SHARED_STOREFRONT_ADDRESS) {
   //   params.append('asset_contract_address', process.env.CONTRACT_ADDRESS!)
   // }
   console.log("Fetching https://api.opensea.io/api/v1/events?" + params);
   const openSeaResponse = await fetch(
-    "https://api.opensea.io/api/v1/events?" + params).then((resp) => resp.json());
+    "https://api.opensea.io/api/v1/events?" + params, { method: 'GET', headers: headers }).then((resp) => resp.json());
   console.log(openSeaResponse);
   return await Promise.all(
     openSeaResponse?.asset_events?.reverse().map(async (sale: any) => {
       const message = buildMessage(sale);
       return channel.send(message)
     })
-  );   
+  );
 }
 
 main()
-  .then((res) =>{ 
+  .then((res) => {
     if (!res.length) console.log("No recent sales")
     process.exit(0)
   })
